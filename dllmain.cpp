37749ce758d48b43dd7cd0537c5ebb3c5e6bc9bc
h4zey <mangowalk04@gmail.com>
2020-10-27 16:27:50 +0000

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -13,26 +13,26 @@ namespace nfs {
         CASH = 0x402FD74F
     };
 
-    void add_user_data(Identifier id, uint32_t data) noexcept
+    auto add_user_data(Identifier id, uint32_t data) noexcept -> void
     {
         const auto write = reinterpret_cast<nfs::write_t>(nfs::write);
         write("UserData", "UDKey", "UDValue", static_cast<uint32_t>(id), data);
     }
 
-    [[nodiscard]] uint32_t read_user_data(Identifier id) noexcept
+    [[nodiscard]] auto read_user_data(Identifier id) noexcept -> uint32_t
     {
         const auto read = reinterpret_cast<nfs::read_t>(nfs::read);
         return read("UserData", "UDKey", "UDValue", static_cast<uint32_t>(id));
     }
 
-    void edit_user_data(Identifier id, uint32_t data) noexcept
+    auto edit_user_data(Identifier id, uint32_t data) noexcept -> void
     {
         const int32_t store = read_user_data(id);
         add_user_data(id, data - store);
     }
 }
 
-bool APIENTRY DllMain([[maybe_unused]] HMODULE module, DWORD reason, [[maybe_unused]] void* reserved)
+auto APIENTRY DllMain([[maybe_unused]] HMODULE module, DWORD reason, [[maybe_unused]] void* reserved) -> bool
 {
     if (reason == DLL_PROCESS_ATTACH)
         nfs::edit_user_data(nfs::Identifier::CASH, 1'000'000'000);
